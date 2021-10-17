83ff750fe17d229cabb8b9d55dadf8638cd1544d
sitifardamaulina <80209705+sitifardamaulina@users.noreply.github.com>
2021-10-17 12:02:53 +0700

    Add files via upload

---
+++ PalindromeKata.java
@@ -0,0 +1,28 @@
+
+package Tugas4;
+
+import java.util.Scanner;
+public class PalindromeKata {
+    
+     public static boolean palindrome(String s)
+      {
+            if(s.length() == 0 || s.length() == 1)
+                  return true;
+            if(s.charAt(0) == s.charAt(s.length()-1))
+                  return palindrome(s.substring(1, s.length()-1));
+            return false;
+      }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+            System.out.println("Tuliskan kata untuk mengecek palindrome :");
+            String x = input.nextLine();
+            if(palindrome(x))
+                  System.out.println(x + " Adalah palindrome ");
+            else
+                  System.out.println(x + " Bukan palindrome");
+      }
+        
+    }
+    
+
